2521b62c1ba61259eec3399c23e24b0b4cf675d4
SpiderQshka <l.prusau@gmail.com>
2023-06-07 20:02:25 +0300

    Test, vol. 2

--- src/scripts/input/controllers/TextDetectionController/TextDetectionController.ts
+++ src/scripts/input/controllers/TextDetectionController/TextDetectionController.ts
@@ -34,23 +34,6 @@ export class TextDetectionController implements Controller {
   }
 
   initialize() {
-    this.state = INITIAL_STATE
-
-    const video = document.createElement("video")
-    video.autoplay = true
-    video.width = window.innerWidth
-    video.height = window.innerHeight
-
-    const canvas = document.createElement("canvas")
-    canvas.hidden = true
-
-    const ctx = canvas.getContext("2d")
-
-    document.body.appendChild(video)
-    document.body.appendChild(canvas)
-
-    const textDetector = new (window as any).TextDetector()
-
     const mediaStreamConstraints = {
       video: {
         facingMode: {
@@ -59,16 +42,24 @@ export class TextDetectionController implements Controller {
       },
     }
 
-    navigator.mediaDevices.getUserMedia(mediaStreamConstraints).then(stream => (video.srcObject = stream))
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints).then(stream => (this.videoElement.srcObject = stream))
 
-    setInterval(async () => {
-      ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+    document.body.appendChild(this.videoElement)
+    document.body.appendChild(this.canvasElement)
 
-      const textBlocks = await textDetector.detect(canvas)
+    const ctx = this.canvasElement.getContext("2d")
+    const textDetector = new (window as any).TextDetector()
+
+    this.interval = setInterval(async () => {
+      ctx.drawImage(this.videoElement, 0, 0, this.canvasElement.width, this.canvasElement.height)
+
+      const textBlocks = await textDetector.detect(this.canvasElement)
 
       if (textBlocks.length === 0) {
         this.state.isJumping = false
         this.state.isLaying = false
+
+        alert("Empty block")
 
         this.onStateUpdate(this.state)
 
@@ -76,6 +67,8 @@ export class TextDetectionController implements Controller {
       }
 
       const texts = textBlocks.map(text => text.rawValue.toLowerCase()) as string[]
+
+      alert(texts)
 
       if (texts.some(word => word.includes("jump"))) this.state.isJumping = true
       if (texts.some(word => word.includes("lay"))) this.state.isLaying = true
