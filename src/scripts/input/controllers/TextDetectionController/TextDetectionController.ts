65f8046f7a472cbe0fa514202900b3a3e19cc954
SpiderQshka <l.prusau@gmail.com>
2023-06-08 20:09:50 +0300

    Test

--- src/scripts/input/controllers/TextDetectionController/TextDetectionController.ts
+++ src/scripts/input/controllers/TextDetectionController/TextDetectionController.ts
@@ -12,62 +12,86 @@ export class TextDetectionController implements Controller {
   constructor() {
     this.playerState = INITIAL_PLAYER_STATE
 
-    this.videoElement = document.createElement("video")
-    this.videoElement.autoplay = true
-    this.videoElement.width = window.innerWidth
-    this.videoElement.height = window.innerHeight
+    // this.videoElement = document.createElement("video")
+    // this.videoElement.autoplay = true
+    // this.videoElement.width = window.innerWidth
+    // this.videoElement.height = window.innerHeight
 
-    this.canvasElement = document.createElement("canvas")
-    this.canvasElement.hidden = true
+    // this.canvasElement = document.createElement("canvas")
+    // this.canvasElement.hidden = true
   }
 
-  initialize() {
-    const mediaStreamConstraints = {
-      video: {
-        facingMode: {
-          exact: "user",
-        },
-      },
-    }
+  async initialize() {
+    this.canvasElement = document.createElement("canvas")
+    // this.canvasElement.hidden = true
 
-    navigator.mediaDevices.getUserMedia(mediaStreamConstraints).then(stream => (this.videoElement.srcObject = stream))
-
-    document.body.appendChild(this.videoElement)
     document.body.appendChild(this.canvasElement)
 
-    const ctx = this.canvasElement.getContext("2d")
     const faceDetector = new (window as any).FaceDetector()
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: "environment" },
+    })
 
-    this.interval = setInterval(async () => {
-      ctx.drawImage(this.videoElement, 0, 0, this.canvasElement.width, this.canvasElement.height)
+    const video = document.createElement("video")
+    video.srcObject = mediaStream
+    video.autoplay = true
+    video.onloadedmetadata = () => {
+      this.canvasElement.width = video.videoWidth
+      this.canvasElement.height = video.videoHeight
+    }
 
-      const faces = await faceDetector.detect(this.canvasElement)
-
-      // if(faces.length === 0){
-      //   alert("No faces detected")
-
-      //   return
-      // }
-
-      alert(faces.length)
-
-      // if (textBlocks.length === 0) {
-      //   this.playerState.isLeaping = false
-      //   this.playerState.isCrouching = false
-
-      //   this.onPlayerStateUpdate(this.playerState)
-
-      //   return
-      // }
-
-      // const texts = textBlocks.map(text => text.rawValue.toLowerCase()) as string[]
-
-      // if (texts.some(word => word.includes("leap"))) this.playerState.isLeaping = true
-      // if (texts.some(word => word.includes("crouch"))) this.playerState.isCrouching = true
-
-      // this.onPlayerStateUpdate(this.playerState)
-    }, 3000)
+    setInterval(() => {
+      faceDetector.detect(video).then(faces => alert(faces.length))
+    }, 2000)
   }
+
+  // initialize() {
+  //   const mediaStreamConstraints = {
+  //     video: {
+  //       facingMode: {
+  //         exact: "user",
+  //       },
+  //     },
+  //   }
+
+  //   navigator.mediaDevices.getUserMedia(mediaStreamConstraints).then(stream => (this.videoElement.srcObject = stream))
+
+  //   document.body.appendChild(this.videoElement)
+  //   document.body.appendChild(this.canvasElement)
+
+  //   const ctx = this.canvasElement.getContext("2d")
+  //   const faceDetector = new (window as any).FaceDetector()
+
+  //   this.interval = setInterval(async () => {
+  //     ctx.drawImage(this.videoElement, 0, 0, this.canvasElement.width, this.canvasElement.height)
+
+  //     const faces = await faceDetector.detect(this.canvasElement)
+
+  //     // if(faces.length === 0){
+  //     //   alert("No faces detected")
+
+  //     //   return
+  //     // }
+
+  //     alert(faces.length)
+
+  //     // if (textBlocks.length === 0) {
+  //     //   this.playerState.isLeaping = false
+  //     //   this.playerState.isCrouching = false
+
+  //     //   this.onPlayerStateUpdate(this.playerState)
+
+  //     //   return
+  //     // }
+
+  //     // const texts = textBlocks.map(text => text.rawValue.toLowerCase()) as string[]
+
+  //     // if (texts.some(word => word.includes("leap"))) this.playerState.isLeaping = true
+  //     // if (texts.some(word => word.includes("crouch"))) this.playerState.isCrouching = true
+
+  //     // this.onPlayerStateUpdate(this.playerState)
+  //   }, 3000)
+  // }
 
   cleanup() {
     document.body.removeChild(this.videoElement)
