db8417a81da65f00c3071283d2cb3f522bd0bd3d
SpiderQshka <l.prusau@gmail.com>
2023-06-14 19:25:57 +0300

    Refactor

--- src/scripts/output/gameConfig.ts
+++ src/scripts/output/gameConfig.ts
@@ -1,4 +1,4 @@
-export const INITIAL_SPAWN_TIMER = 200
-export const GAME_SPEED = 8
+export const INITIAL_SPAWN_TIMER = 175
+export const GAME_SPEED = 10
 export const GRAVITY = 1.4
 export const LEAP_FORCE = 35
